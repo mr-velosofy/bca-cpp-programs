7f5fc64f80cf965a54eb7bb0b6261131f2de5a6e
mr-velosofy <114012244+mr-velosofy@users.noreply.github.com>
2022-09-23 23:00:34 +0530

    Update Fibonacci-Series.cpp

---
+++ Looping Programs/Fibonacci-Series.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a = 0, b = 1, c = 0;
+
+    cout << "Enter the number of terms : ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+    int i=1;
+
+    while (i <= n) {
+        // Prints the first two terms.
+        if(i == 1) {
+            cout << a << "  ";
+        }
+        if(i == 2) {
+            cout << b << "  ";
+        }
+        c = a+b;
+        a = b;
+        b = c;
+        
+        cout << c << "  ";
+        i++;
+    }
+    return 0;
+}
