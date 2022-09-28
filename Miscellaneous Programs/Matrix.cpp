feee3a437b8987f14934c821a46dc3cabf8abe5d
mr-velosofy <114012244+mr-velosofy@users.noreply.github.com>
2022-09-28 22:57:50 +0530

    for matrixes w/ unique rows and columns

---
+++ Miscellaneous Programs/Matrix.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#define clrscr(); system("cls");
+using namespace std;
+
+int main(){
+
+    int r,c,i,j;
+
+
+    cout<<"Enter Rows of Matrix: ";
+    cin>>r;
+
+    cout<<"Enter Columns of Matrix : ";
+    cin>>c;
+
+    int a[r][c],trans[r][c];
+
+    for (i = 1; i<=r ; i++)
+    {
+        for (j = 1; j<= c ; j++)
+        {
+            cout<<"Enter A["<<i<<"]["<<j<<"] : ";
+            cin>>a[i][j];
+        }
+    } 
+
+
+    //printing the matrix
+    cout<<"\n---MATRIX A---\n";
+
+    for (i = 1; i<=r ; i++)
+    {
+        for (j = 1; j<= c ; j++)
+        {
+            cout<<a[i][j]<<"\t|\t";
+        }
+        cout<<endl;
+    }
+
+}
